eb836357e1c0729ecae9cb6f42b45d32031c4c86
Danijel Vilovski <danivilovski@gmail.com>
2022-10-06 11:11:25 +0200

    Finished 'edit functionality', added new form.

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='homeview'),
     path('blogs/<int:pk>', views.BlogsDetailView.as_view(), name='blogdetailview'),
-    path('addblog', views.AddBlogView.as_view(), name='addblogview')
+    path('addblog', views.AddBlogView.as_view(), name='addblogview'),
+    path('blogs/updateblog/<int:pk>', views.EditBlogView.as_view(), name='updateblogview')
 ]
