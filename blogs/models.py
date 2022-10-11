c8e566f5342c17d51be9aa0eb8fd57e7e63a3f51
Danijel Vilovski <danivilovski@gmail.com>
2022-10-11 14:56:18 +0200

    Installed ckeditor and added richtext to add and edit forms. Fixed authorization.

--- blogs/models.py
+++ blogs/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -15,7 +16,8 @@ class Category(models.Model):
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    #body = models.TextField()
     date_created = models.DateField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
  
