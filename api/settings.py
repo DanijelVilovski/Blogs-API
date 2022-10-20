e848d952842a45c0b05a8c42827807e20974ca22
Danijel Vilovski <danivilovski@gmail.com>
2022-10-20 16:23:34 +0200

    Setted database credentials.

--- api/settings.py
+++ api/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'api.urls'
@@ -89,10 +93,10 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogs-database',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost'
+        'NAME': 'd6188qgpum971l',
+        'USER': 'wyepewwtsbrbkv',
+        'PASSWORD': '1995196819c6a78896aaa313c5072700d5c2445d44d00da98acf37c98d1d2806',
+        'HOST': 'ec2-54-160-200-167.compute-1.amazonaws.com'
     }
 }
 
@@ -140,10 +144,14 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'homeview'
-LOGOUT_REDIRECT_URL = 'login'   
+LOGOUT_REDIRECT_URL = 'login'  
+
+django_heroku.settings(locals())
